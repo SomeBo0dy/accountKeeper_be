1036c3a3016885236e6ffbbed35eb6f38a2ededb
浪里小白虫… <xyj18105857856@163.com>
2024-02-17 21:51:20 +0800

    Initial commit

---
+++ src/main/java/pers/xyj/AccountKeeperApplication.java
@@ -0,0 +1,13 @@
+package pers.xyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountKeeperApplication.class, args);
+    }
+
+}
